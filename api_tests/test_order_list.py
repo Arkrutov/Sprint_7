b276dad84866f7561388a0db9e9fec119934a11f
arkrutov <arkrutov@ozon.ru>
2025-02-02 20:11:50 +0300

    Sprint_7

---
+++ api_tests/test_order_list.py
@@ -0,0 +1,12 @@
+import allure
+import requests
+
+class TestLogIn:
+    @allure.title("В тело ответа возвращается список заказов")
+    def test_order_list_return_list(self):
+        with allure.step("Отправляем запрос на получения списка заказов"):
+            response = requests.get('https://qa-scooter.praktikum-services.ru/api/v1/orders?limit=10&page=0&nearestStation=["110"]')
+            response_data = response.json()
+        with allure.step("Проверяем, что в тело ответа возвращается список заказов"):
+            assert 'orders' in response_data
+            assert isinstance(response_data['orders'], list)
